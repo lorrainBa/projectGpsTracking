6edfc1b0a8e73a9430f233bcb0a0081e9cb60767
LorrainBa <ordure.algebrique@gmail.com>
2024-01-17 16:21:47 +0100

    dockerfile à jour

--- producer/launchDemonstration.py
+++ producer/launchDemonstration.py
@@ -1,6 +1,5 @@
 from createCoord import getCoordKebab, calculateNewCoord
 from sendCoordToKafka import produce_message
-from readBrokerKafka import consume_messages
 from math import sqrt
 import sys
 import json
@@ -31,16 +30,17 @@ if __name__ == "__main__" :
     #While there is kebab to visit
     while coordsKebab:
         nomKebab, destination = coordsKebab.popitem()
-        print("ARRIVEE KEBAB")
+        
         print('destination',destination)
         #Continue to go to the kebab if you're not near
         while (sqrt(((currentCoord[0]-destination[0])**2 + (currentCoord[1]-destination[1])**2))) > 0.00002:
             currentCoordToSend= calculateNewCoord(currentCoord, destination, speed = 1 )
-            # Produce the message to Kafka
-
+            
+            # Produce the message to Kafka and put it in a string
             messageToSend = {"ip":numProducer,"latitude":currentCoordToSend[0],"longitude":currentCoordToSend[1],"nomKebab":nomKebab}
             messageToSend = json.dumps(messageToSend)
 
             print(messageToSend)
             produce_message(bootstrap_servers, kafka_topic, messageToSend)
+        print("ARRIVEE KEBAB")
         print("currentCoord",currentCoord)
