008081ae5a6764914b481022b7ea1fab78c03b8f
IRamNoc <90531307+IRamNoc@users.noreply.github.com>
2024-01-26 10:08:47 +0100

    consumer update

--- consumer/consumer.js
+++ consumer/consumer.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-const Kafka = require('node-rdkafka');
+const kafka = require('kafka-node');
 
 const dbConfig = {
   user: 'trackinguser',
@@ -11,55 +11,50 @@ const dbConfig = {
 
 const client = new Client(dbConfig);
 
-async function consumeMessages(bootstrapServers, groupId, topic) {
-  const consumer = new Kafka.KafkaConsumer(
+async function consumeMessages(bootstrapServers, topic) {
+  const client = new kafka.KafkaClient({ kafkaHost: bootstrapServers });
+  const consumer = new kafka.Consumer(
+    client,
+    [{ topic: topic, partition: 0 }],
     {
-      'group.id': groupId,
-      'metadata.broker.list': bootstrapServers,
-      'auto.offset.reset': 'earliest',
-    },
-    {}
+      autoCommit: true,
+      autoCommitIntervalMs: 5000,
+      fromOffset: 'earliest'
+    }
   );
 
-  consumer.connect();
+  consumer.on('message', async function (message) {
+    try {
+      // Traitez les coordonnées reçues
+      const coordinates = JSON.parse(message.value);
+      console.log(coordinates);
+      // Connectez-vous à la base de données
+      await client.connect();
 
-  consumer
-    .on('ready', function () {
-      console.log('Consumer is ready.');
+      // Ajoutez les données à la base de données sous forme JSON
+      const insertQuery = 'INSERT INTO coords (ip, latitude, longitude, nomLieu) VALUES ($1, $2, $3, $4);';
+      await client.query(insertQuery, [JSON.stringify(coordinates)]);
 
-      consumer.subscribe([topic]);
-      consumer.consume();
-    })
-    .on('data', async function (message) {
-      try {
-        // Traitez les coordonnées reçues
-        const coordinates = JSON.parse(message.value.toString());
-        console.log(coordinates);
-        // Connectez-vous à la base de données
-        await client.connect();
+      console.log('Received and stored message:', message.value);
+    } catch (error) {
+      console.error('Error processing message:', error);
+    }
+  });
 
-        // Ajoutez les données à la base de données sous forme JSON
-        const insertQuery = 'INSERT INTO coords (ip, latitude, longitude, nomLieu) VALUES ($1, $2, $3, $4);';
-        await client.query(insertQuery, [JSON.stringify(coordinates)]);
-
-        console.log('Received and stored message:', message.value.toString());
-      } catch (error) {
-        console.error('Error processing message:', error);
-      }
-    })
-    .on('error', function (err) {
-      console.error('Error:', err);
-    });
+  consumer.on('error', function (err) {
+    console.error('Error:', err);
+  });
 
   process.on('SIGINT', function () {
-    consumer.disconnect();
-    client.end();
+    consumer.close(true, function () {
+      client.end();
+      process.exit();
+    });
   });
 }
 
 // Utilisation
-const bootstrapServers = '172.21.0.2:9092'; // Utilisez le nom du service Kafka dans le réseau Docker
-const groupId = 'gps_consumer_group';
+const bootstrapServers = 'kafka:9092';
 const topic = 'coordinates';
 
-consumeMessages(bootstrapServers, groupId, topic);
+consumeMessages(bootstrapServers, topic);
